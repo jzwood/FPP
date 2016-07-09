aa5b46f2416d78fe54fd9b893bac8c5c17c4c58e
Jake Wood <jwood999@apple.com>
2016-07-08 23:50:57 -0700

    page transitions

--- src/assets/js/mouseHandling.js
+++ src/assets/js/mouseHandling.js
@@ -6,19 +6,30 @@ var l0, l1, l2, c_boxes
 window.onload = function() {
   l0 = document.querySelector('.layer0'),
   l1 = document.querySelector('.layer1'),
+  c1 = document.querySelector('.content1'),
   l2 = document.querySelector('.layer2'),
   c_boxes = document.querySelectorAll('.c_box')
 }
 
 function howto() {
+
+  if(levels) showLevels()
+
   l1.classList.toggle("minified")
+  var body = document.body
+  body.style.overflow = 'hidden'
   var showlevels = setTimeout(function() {
-    document.body.classList.toggle('layer1')
+    body.classList.toggle('layer1')
+    c1.classList.toggle('hide')
+    body.style.overflow = 'visible'
   }, instructions ? 0 :1000)
   instructions = !instructions
 }
 
 function showLevels() {
+
+  if(instructions) howto()
+
   l2.classList.toggle("minified")
 
   var showlevels = setTimeout(function() {
@@ -27,7 +38,6 @@ function showLevels() {
     })
     document.body.classList.toggle('layer2')
   }, levels ? 0 : 1000)
-
   levels = !levels
 
 }
