2a1517c84aaacb550c0b96467a494dad75c47ac2
Jake Wood <jwood999@apple.com>
2016-07-10 11:29:01 -0700

    cleaned up homepage tab switching. updated some copy.

--- src/assets/js/mouseHandling.js
+++ src/assets/js/mouseHandling.js
@@ -1,9 +1,11 @@
 var instructions = false,
-levels = false
+levels = false,
+animating = false
 
 var l0, l1, l2, c_boxes
 
 window.onload = function() {
+  body = document.body,
   l0 = document.querySelector('.layer0'),
   l1 = document.querySelector('.layer1'),
   c1 = document.querySelector('.content1'),
@@ -11,24 +13,36 @@ window.onload = function() {
   c_boxes = document.querySelectorAll('.c_box')
 }
 
-function howto() {
+function home(){
+  if(!animating){
+    if(levels) showLevels()
+    if(instructions) howto()
+  }
+}
 
+function howto() {
   if(levels) showLevels()
 
+  // if(!animating){
+  animating = true
   l1.classList.toggle("minified")
-  var body = document.body
-  body.style.overflow = 'hidden'
+  body.classList.toggle('scroll-lock')
   var showlevels = setTimeout(function() {
     body.classList.toggle('layer1')
     c1.classList.toggle('hide')
-    body.style.overflow = 'visible'
+    body.classList.toggle('scroll-lock')
+    animating = false
   }, instructions ? 0 :1000)
   instructions = !instructions
+  // }
 }
 
 function showLevels() {
 
   if(instructions) howto()
+
+  // if(!animating){
+  animating = true
 
   l2.classList.toggle("minified")
 
@@ -37,7 +51,10 @@ function showLevels() {
       box.classList.toggle('hide')
     })
     document.body.classList.toggle('layer2')
+    animating = false
+
   }, levels ? 0 : 1000)
   levels = !levels
+  // }
 
 }
