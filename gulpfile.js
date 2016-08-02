2e57a564d32a10e6cac97a10e361812cecbce3b8
Jake Wood <jwood999@apple.com>
2016-08-02 09:34:21 -0700

    adding build infrastructure for autoprefixing and dev local server.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+autoprefixer = require('gulp-autoprefixer'),
+connect = require('gulp-connect')
+
+gulp.task('default', function () {
+    return gulp.src('src/app.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 4000,
+    root: 'src'
+  });
+})
